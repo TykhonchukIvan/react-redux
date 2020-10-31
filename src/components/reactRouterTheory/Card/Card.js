da045880b55ceed2f9b51a4a387fc5e0bd24e1b4
Tykhonchuk <tykhonchukivan@gmail.com>
2020-10-31 13:08:10 +0200

    feat: rout v2.0

--- src/components/reactRouterTheory/Card/Card.js
+++ src/components/reactRouterTheory/Card/Card.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import CardBox from './CardBox/CardBox.jsx';
 
 class Card extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      cars: [
+        {name: 'bmw', year: '2020'},
+        {name: 'audi', year: '2019'},
+        {name: 'mazda', year: '2028'},
+      ]
+    }
+    this.goToHomePage = this.goToHomePage.bind(this)
+  }
+
+  goToHomePage () {
+    this.props.history.push({
+      pathname: '/'
+    })
   }
 
   render() {
-    console.log('4', this.props)
 
-    const styles = {
-      width: '200px',
-      height: '200px',
-      border: '1px solid #ccc',
-      margin: '10px auto',
-      background: '#000283'
-    }
+    console.log(this.props)
+
+    const cars = this.state.cars.map((car, index) =>
+      <CardBox key={index} name={car.name} year={car.year} />
+    )
 
     return (
-      <div style={styles}>
-
+      <div style={{
+        textAlign: 'center'
+      }}>
+        <hr/>
+        <button onClick={this.goToHomePage}>Click home</button>
+        <hr/>
+        {cars}
       </div>
     )
   }
--- src/components/reactRouterTheory/index.js
+++ src/components/reactRouterTheory/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {Route, NavLink} from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 import About from './About/About';
 import Card from './Card/Card';
+import CarDetail from './CarDetail/CarDetail.jsx';
 import './routStyle.less';
 
 export default class RoutTheory extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      isLoggedIn: false
+    }
   }
 
   render() {
@@ -36,9 +40,9 @@ export default class RoutTheory extends React.Component {
               <li>
                 <NavLink
                   to={{
-                    pathname:'/cars',
-                    search:'?a=1&b=2',
-                    hash:'wfm-hash'
+                    pathname: '/cars',
+                    // search:'?a=1&b=2',
+                    // hash:'wfm-hash'
                   }}
                   exact
                 >Cars</NavLink>
@@ -46,11 +50,19 @@ export default class RoutTheory extends React.Component {
             </ul>
           </nav>
           <hr/>
-
-          <Route path='/' exact render={() => <h1>Home</h1>}/>
-          <Route path='/about' exact component={About}/>
-          <Route path='/cars' exact component={Card}/>
-
+          <div style={{textAlign:'center'}}>
+            <h3>Is logged in {this.state.isLoggedIn ? 'true':'false'}</h3>
+            <button onClick={()=>this.setState({isLoggedIn: true})}>Log in</button>
+          </div>
+          <hr/>
+          <Switch>
+            <Route path='/' exact render={() => <h1>Home</h1>}/>
+            {this.state.isLoggedIn ? <Route path='/about' component={About}/> : null}
+            <Route path='/cars/:name' component={CarDetail}/>
+            <Route path='/cars' component={Card}/>
+            <Redirect to={'/'}/>
+            {/*<Route render={()=><h1 style={{color:'red'}}>404</h1>}/>*/}
+          </Switch>
         </div>
       </>
 
